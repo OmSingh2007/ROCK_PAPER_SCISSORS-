ff46e45fcc467e308e5c789e9b98ba5925fe9929
OmSingh2007 <osingh3639@gmail.com>
2025-12-06 21:49:36 +0530

    This are the images source code of rock paper scissors game

---
+++ app.js
@@ -0,0 +1,64 @@
+let selection=document.querySelectorAll(".choice");
+let choice=['Rock','Paper','Scissor'];
+let yourscore=0,compscore=0;
+const computer = (choice) =>{
+    let randomchoice=choice[Math.floor(Math.random()*choice.length)];
+    return randomchoice;
+}
+selection.forEach( (btn , index) => {
+    btn.onclick=()=>{
+        let playerchoice=choice[index];
+        let computerchoice=computer(choice);
+
+        let msg=document.querySelector("#msg");
+        let yourscoreEL=document.querySelector("#yourscore");
+        let compscoreEL=document.querySelector("#compscore");
+        // For draw
+        if(playerchoice===computerchoice){
+            msg.textContent="It's a Draw";
+            msg.style.backgroundColor="grey";
+
+        }
+        //Rock
+        else if(playerchoice==="Rock"){
+           if(computerchoice==="Paper"){
+            msg.textContent="You Lose , Computer Choose Paper";
+            msg.style.backgroundColor="Red";
+            compscore++;
+           } 
+           else{
+            msg.textContent="You Win , Computer Choose Scissor";
+            msg.backgroundColor="Blue";
+            yourscore++;
+           }
+        }
+        //Paper
+        else if(playerchoice==="Paper"){
+            if(computerchoice==="Scissor"){
+                msg.textContent="You Lose , Computer Choose Scissor"
+                msg.style.backgroundColor="Red";
+                compscore++;
+            }
+            else{
+                msg.textContent="You Win , Computer Choose Rock";
+                msg.style.backgroundColor="Blue";
+                yourscore++;
+            }
+        }
+        //Scissor
+        else if(playerchoice==="Scissor"){
+            if(computerchoice==="Rock"){
+                msg.textContent="You Lose , Computer Choose Rock"
+                msg.style.backgroundColor="Red";
+                compscore++;
+            }
+            else{
+                msg.textContent="You Win , Computer Choose Paper";
+                msg.style.backgroundColor="Blue";
+                yourscore++;
+            }
+        }
+        yourscoreEL.textContent=yourscore;
+        compscoreEL.textContent=compscore;
+    };
+});
